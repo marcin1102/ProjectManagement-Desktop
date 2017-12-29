47512efeda35b0f0acbaf8929f0ea86544b02f96
Marcin Gurbiel <gurbielmarcin@gmail.com>
2017-12-29 15:15:20 +0100

    Unhandled exception handling

--- ProjectManagement/ProjectManagement/App.xaml.cs
+++ ProjectManagement/ProjectManagement/App.xaml.cs
@@ -13,5 +13,21 @@ namespace ProjectManagement
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string message;
+            if (string.IsNullOrWhiteSpace(e.Exception.InnerException.Message))
+                message = e.Exception.Message;
+            else
+                message = e.Exception.InnerException.Message;
+
+            MessageBox.Show("An unhandled exception just occurred: " + message, "Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
     }
 }
